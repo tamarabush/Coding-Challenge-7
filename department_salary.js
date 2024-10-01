c0d987bec0c1e1671ead76ebdd6612791d77d9e9
tamarabush <tamara.abush14@outlook.com>
2024-09-30 22:05:17 -0400

    Calculate total salary for all departments in the company

--- department_salary.js
+++ department_salary.js
@@ -61,7 +61,7 @@ function calculateDepartmentSalary(departments) {
     let sumOfSalaries = 0
     //starting the calculation off with a 0 to calculate the sum of the salaries
 
-    for (const characterEmployee of departments.employees) {
+    for (let characterEmployee of departments.employees) {
         sumOfSalaries += characterEmployee.salary;
     //using the for of loop to iterate over the employees to accumlate their salaries
 
@@ -69,12 +69,12 @@ function calculateDepartmentSalary(departments) {
         sumOfSalaries += calculateDepartmentSalary({employees: characterEmployee.subordinates});
     //using an if statement to check if that employee has subordinates
     //if they do, then it will calculate the total salaries of those subordinates
-    }
- }
+    }}
 
     return sumOfSalaries;
     //returning the total of all salaries
 }
+
 let businessTotalSalary = calculateDepartmentSalary(company.departments[0]);
 console.log(`The total salary for the Business Department is $${businessTotalSalary}.`);
 //outputs the total salary for the business department
@@ -84,3 +84,21 @@ console.log(`The total salary for the Civil Engineering Department is $${enginee
 //outputs the total salary for the civil engineering department
 
 
+
+//TASK 3 - Create a Function to Calculate the Total Salary for All Departments:
+
+function calculateCompanySalary(company) {
+    let totalSalaryCompany = 0;
+    //starting the calculation off with a 0 to calculate the sum of Comapny salaries
+
+    company.departments.forEach(department => {
+        totalSalaryCompany += calculateDepartmentSalary(department);
+    //using the for each method to iterate through the company department and calculate the total 
+    });
+
+    return totalSalaryCompany;
+    //returning the total salaries for the comapny 
+}
+
+let totalOfCompanySalaries = calculateCompanySalary(company);
+console.log(`The total salary for the company is $${totalOfCompanySalaries}.`);
